b11b2a854b2b0a79281624f2d335e2edf3ad56e0
Genadiy-dot <gem1@mail.ru>
2020-11-04 00:26:35 +0400

    решил задачу 6  убрал условие проверки пользовательского ввода,иначе не вводятся вещественные числа

--- task6.py
+++ task6.py
@@ -7,21 +7,15 @@
   Программа должна принимать значения параметров a и b и выводить
 одно натуральное число — номер дня.
 '''
-while True:
 
-    user_a = input('Введите переменную а: ')
-    user_b = input('Введите перемееную b: ')
-    day = 1
+user_a = float(input('Введите переменную а: '))
+user_b = float(input('Введите перемееную b: '))
+day = 1
 
-    if user_a.isdigit() and user_b.isdigit() and user_a[0] != '0' and  user_b[0] != '0':
-        user_a = int(user_a)
-        user_b = int(user_b)
+while user_a < user_b:
 
-        while user_a <= user_b:
-            user_a *= 1.1 # увеличение числа а на 10 процентов
-            day += 1
+    user_a *= 1.1  # увеличение числа а на 10 процентов
+    day += 1
 
-        print(day)
+print(day)
 
-    else:
-        print('Ошибка ввода')
